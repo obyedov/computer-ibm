24386b6416313bd91aedc5a8c41bf235b6c06e8a
Vitaliy Obyedov <obyedov@gmail.com>
2024-10-28 10:49:17 +0100

    I solved my first project in Java.

---
+++ src/com/obyedov/oop/computer/Computer.java
@@ -0,0 +1,13 @@
+package com.obyedov.oop.computer;
+
+public class Computer {
+    String name;
+
+    void on(){
+        System.out.println(name + " = acceso");
+    }
+
+    void off(){
+        System.out.println(name + " = spento");
+    }
+}
---
+++ src/com/obyedov/oop/computer/Main.java
@@ -0,0 +1,11 @@
+package com.obyedov.oop.computer;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        computer.name = "IBM";
+
+        computer.on();
+        computer.off();
+    }
+}
